908a2ed96e2b689e4f9bbec9bc0b35e171c18a00
schumann7 <gabrielschumannsc@gmail.com>
2025-05-09 09:36:00 -0300

    pouca estilização

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: 'Etanômico',
-    theme: ThemeData(
-      primarySwatch: Colors.green,
-      visualDensity: VisualDensity.adaptivePlatformDensity,
+  runApp(
+    MaterialApp(
+      title: 'Etanômico',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const TelaInicial(),
+      debugShowCheckedModeBanner: false,
     ),
-    home: const TelaInicial(),
-    debugShowCheckedModeBanner: false,
-  ));
+  );
 }
 
 class TelaInicial extends StatelessWidget {
@@ -22,44 +24,78 @@ class TelaInicial extends StatelessWidget {
         leading: Image.asset('assets/images/Logo_Etanômico.png'),
         title: Text('Etanômico'),
       ),
-      body: Center(
-        child: ListView(
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const TelaComparativo(),
-                  ),
-                );
-              },
-              child: const Text('Calculadora Álcool x Gasolina'),
+      body: ListView(
+        padding: const EdgeInsets.only(left: 30, right: 30, top: 30),
+        children: [
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              padding: const EdgeInsets.only(top: 20, bottom: 20),
+              backgroundColor: Colors.white,
+              side: const BorderSide(
+                color: Colors.black,
+                width: 1,
+              ),
             ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const TelaConsumo(),
-                  ),
-                );
-              },
-              child: const Text('Calculadora de Consumo'),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const TelaComparativo(),
+                ),
+              );
+            },
+            child: const Text(
+              'Calculadora Álcool x Gasolina',
+              style: TextStyle(color: Colors.black),
             ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const TelaPrecos(),
-                  ),
-                );
-              },
-              child: const Text('Monitoraramento de Preços'),
-            ),  
-          ],
-        ),
+          ),
+          const SizedBox(height: 30),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              padding: const EdgeInsets.only(top: 20, bottom: 20),
+              backgroundColor: Colors.white,
+              side: const BorderSide(
+                color: Colors.black,
+                width: 1,
+              ),
+            ),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const TelaConsumo(),
+                ),
+              );
+            },
+            child: const Text(
+              'Calculadora de Consumo',
+              style: TextStyle(color: Colors.black),
+            ),
+          ),
+          const SizedBox(height: 30),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              padding: const EdgeInsets.only(top: 20, bottom: 20),
+              backgroundColor: Colors.white,
+              side: const BorderSide(
+                color: Colors.black,
+                width: 1,
+              ),
+            ),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const TelaPrecos(),
+                ),
+              );
+            },
+            child: const Text(
+              'Monitoraramento de Preços',
+              style: TextStyle(color: Colors.black),
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -76,18 +112,14 @@ class _TelaComparativoState extends State<TelaComparativo> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Tela Comparativa'),
-      ),
-      body: Center(
-        child: Text('Tela Comparativa'),
-      ),
+      appBar: AppBar(title: Text('Tela Comparativa')),
+      body: Center(child: Text('Tela Comparativa')),
     );
   }
 }
 
 class TelaConsumo extends StatefulWidget {
-  const TelaConsumo ({super.key});
+  const TelaConsumo({super.key});
 
   @override
   State<TelaConsumo> createState() => _TelaConsumoState();
@@ -97,18 +129,14 @@ class _TelaConsumoState extends State<TelaConsumo> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Tela Consumo'),
-      ),
-      body: Center(
-        child: Text('Tela Consumo'),
-      ),
+      appBar: AppBar(title: Text('Tela Consumo')),
+      body: Center(child: Text('Tela Consumo')),
     );
   }
 }
 
 class TelaPrecos extends StatefulWidget {
-  const TelaPrecos ({super.key});
+  const TelaPrecos({super.key});
 
   @override
   State<TelaPrecos> createState() => _TelaPrecosState();
@@ -118,12 +146,8 @@ class _TelaPrecosState extends State<TelaPrecos> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Tela Preços'),
-      ),
-      body: Center(
-        child: Text('Tela Preços'),
-      ),
+      appBar: AppBar(title: Text('Tela Preços')),
+      body: Center(child: Text('Tela Preços')),
     );
   }
 }
