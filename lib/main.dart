5fa6929087000ef96668776e0a47ddb3bb23df1c
schumann7 <gabrielschumannsc@gmail.com>
2025-05-13 14:24:07 -0300

    Tela preços pronta

--- lib/main.dart
+++ lib/main.dart
@@ -225,6 +225,7 @@ class _TelaComparativoState extends State<TelaComparativo> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: const Color.fromARGB(247, 246, 244, 255),
         toolbarHeight: 110,
         leading: IconButton(
           icon: const Icon(
@@ -237,8 +238,6 @@ class _TelaComparativoState extends State<TelaComparativo> {
           },
           tooltip: 'Voltar',
           splashRadius: 24,
-          splashColor: Colors.blueAccent,
-          highlightColor: Colors.lightBlueAccent,
         ),
         centerTitle: true,
         title: Row(
@@ -269,6 +268,11 @@ class _TelaComparativoState extends State<TelaComparativo> {
           ],
         ),
       ),
+      body: Container(
+        decoration: const BoxDecoration(
+          color: Color.fromARGB(247, 246, 244, 255),
+        ),
+      ),
     );
   }
 }
@@ -286,17 +290,31 @@ class _TelaConsumoState extends State<TelaConsumo> {
     return Scaffold(
       appBar: AppBar(
         toolbarHeight: 110,
+        backgroundColor: const Color.fromARGB(247, 246, 244, 255),
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back_rounded,
+            color: Colors.black,
+            size: 28,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          tooltip: 'Voltar',
+          splashRadius: 24,
+        ),
         centerTitle: true,
         title: Row(
           mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
           children: [
             Image.asset('assets/images/Logo_Etanômico.png', height: 60),
-            const SizedBox(width: 2),
+            const SizedBox(width: 3),
             Flexible(
               child: Builder(
                 builder:
                     (context) => Padding(
-                      padding: const EdgeInsets.only(top: 25),
+                      padding: const EdgeInsets.only(top: 35),
                       child: Text(
                         'Etanômico',
                         style: TextStyle(
@@ -314,6 +332,11 @@ class _TelaConsumoState extends State<TelaConsumo> {
           ],
         ),
       ),
+      body: Container(
+        decoration: const BoxDecoration(
+          color: Color.fromARGB(247, 246, 244, 255),
+        ),
+      ),
     );
   }
 }
@@ -326,15 +349,179 @@ class TelaPrecos extends StatefulWidget {
 }
 
 class _TelaPrecosState extends State<TelaPrecos> {
-  final List<String> lista = ["BR", "AL", "AM",	"CE",	"DF",	"ES",	"GO",	"MA",	"MT",	"MG",	"PR",	"PB",	"PA",	"PE",	"RS",	"RJ",	"SC",	"SP"];
+  final List<String> lista = [
+    "BR",
+    "AL",
+    "AM",
+    "CE",
+    "DF",
+    "ES",
+    "GO",
+    "MA",
+    "MT",
+    "MG",
+    "PR",
+    "PB",
+    "PA",
+    "PE",
+    "RS",
+    "RJ",
+    "SC",
+    "SP",
+  ];
   String? _selectedItem;
   String preco = "";
+  String retorno = "";
   final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(centerTitle: true, title: Text('Monitoramento de preços')),
+      appBar: AppBar(
+        toolbarHeight: 110,
+        backgroundColor: const Color.fromARGB(247, 246, 244, 255),
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back_rounded,
+            color: Colors.black,
+            size: 28,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          tooltip: 'Voltar',
+          splashRadius: 24,
+        ),
+        centerTitle: true,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset('assets/images/Logo_Etanômico.png', height: 60),
+            const SizedBox(width: 3),
+            Flexible(
+              child: Builder(
+                builder:
+                    (context) => Padding(
+                      padding: const EdgeInsets.only(top: 35),
+                      child: Text(
+                        'Etanômico',
+                        style: TextStyle(
+                          fontSize:
+                              MediaQuery.of(context).size.width < 400 ? 26 : 34,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 1,
+                        softWrap: false,
+                      ),
+                    ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: Container(
+        color: const Color.fromARGB(247, 246, 244, 255),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                const SizedBox(height: 60),
+                DropdownButtonFormField<String>(
+                  decoration: const InputDecoration(
+                    labelText: 'Selecione seu estado',
+                    border: OutlineInputBorder(),
+                    filled: true,
+                    fillColor: Color.fromARGB(247, 246, 244, 255),
+                  ),
+                  value: _selectedItem,
+                  items:
+                      lista.map((item) {
+                        return DropdownMenuItem(
+                          value: item,
+                          child: Text(
+                            item,
+                            style: const TextStyle(color: Colors.black),
+                          ),
+                        );
+                      }).toList(),
+                  onChanged: (value) {
+                    setState(() {
+                      _selectedItem = value;
+                    });
+                  },
+                  validator: (value) {
+                    if (value == null) {
+                      return 'Selecione um item';
+                    }
+                    return null;
+                  },
+                  dropdownColor: const Color.fromARGB(247, 246, 244, 255),
+                ),
+                const SizedBox(height: 30),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(247, 246, 244, 255),
+                    side: const BorderSide(color: Colors.black, width: 1),
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 18,
+                      horizontal: 30,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                  ),
+                  child: const Text(
+                    'Confirmar',
+                    style: TextStyle(color: Colors.black, fontSize: 15),
+                  ),
+                  onPressed: () async {
+                    if (_selectedItem != null) {
+                      try {
+                        final response = await http.get(
+                          Uri.parse('https://combustivelapi.com.br/api/precos'),
+                          headers: {
+                            'Content-Type': 'application/json; charset=UTF-8',
+                          },
+                        );
+
+                        final json = jsonDecode(response.body);
+
+                        if (json['precos']['gasolina'].containsKey(
+                          _selectedItem!.toLowerCase(),
+                        )) {
+                          setState(() {
+                            preco =
+                                json['precos']['gasolina'][_selectedItem!
+                                        .toLowerCase()]
+                                    .toString();
+                            retorno =
+                                'A gasolina em sua localização custa em média R\$$preco';
+                          });
+                        }
+                      } catch (e) {
+                        setState(() {
+                          retorno =
+                              'Ocorreu um erro na busca do preço \n Por favor, tente novamente mais tarde.';
+                        });
+                      }
+                    }
+                  },
+                ),
+                const SizedBox(height: 30),
+                Text(
+                  retorno != "" ? retorno : "",
+                  style: const TextStyle(fontSize: 19),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
